27aa00d33e60d1b6471015fab13eb6fa517cf90c
Noel Carino <leononirac1992@gmail.com>
2019-10-05 22:47:47 -0700

    10/5/2019 - 10:47pm | Got display.js to display text to the dom

--- js/display.js
+++ js/display.js
@@ -3,6 +3,9 @@ class Display {
 
     }
     testing(){
-        console.log("This is a test");
+        console.log("hello, display.js's console!")
+        var eventContainer = $("#events-container");
+
+        eventContainer.text("Hello i'm finally working :)");
     }
 }
